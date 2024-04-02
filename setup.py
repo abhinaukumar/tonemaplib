8e76a0799dfa727c348fd1128098c7175428e13a
Abhinau Kumar <ab.kumr98@gmail.com>
2024-04-02 15:56:11 -0500

    Setup script

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tonemaplib',
+    author='Abhinau Kumar',
+    author_email='ab.kumr98@gmail.com',
+    version='0.1.0',
+    url='https://github.com/abhinaukumar/tonemaplib',
+    description='Package for HDR video tone mapping in Python.',
+    install_requires=[
+        'PyWavelets',
+        'scikit-image',
+        'protobuf',
+        'tensorflow-gpu',
+        'torch',
+        'torchvision',
+        'joblib',
+        'videolib @ git+https://github.com/abhinaukumar/videolib@main',
+        'opencv-python',
+        'imageio',
+        'easydict',
+        'scipy',
+        'matplotlib',
+        'gdown'
+    ],
+    python_requires='>=3.7.0',
+    license='MIT License',
+    packages=find_packages()
+)
